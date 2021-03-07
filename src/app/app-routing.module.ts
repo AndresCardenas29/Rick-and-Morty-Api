9663cb6042956825c37456fbd2050b0715bebec0
Andrés Cardenas <mandrescardenash@gmail.com>
2021-03-07 12:57:28 -0500

    PruebasTest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { GaleryComponent } from './components/galery/galery.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'gallery', component: GaleryComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
